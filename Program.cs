0c553af01425c1b868b16c836d8994b3cd97634d
Lagutina Elizaveta <below8585@mail.ru>
2023-03-21 09:03:23 +0300

    Созданы метод ввода пользователем массива с клавиатуры и метод красивого его вывода на экран

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// Написать программу, которая из имеющегося массива строк сформирует
+// массив из строк, длина которых меньше либо равна 3 символа
+// Пример: ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+Console.WriteLine("Введите количество элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+// Метод ввода пользователем массива строк с клавиатуры
+string[] array(int size)
+{
+    string[] str = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i} элемент массива: ");
+        str[i] = Console.ReadLine();
+    }
+    return str;
+}
+// Метод вывода массива
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < size - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    for (int i = size - 1; i < size; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+string[] str = array(size);
+PrintArray(str);
